6b74ebba5ddcd99fa7309b20cc6a248a55d3ea8f
Noeek <Noeek@live.cn>
2015-08-23 21:19:10 +0800

    hmmm,guys more perfect
    Now you can save the space char of the html tag's inner text if you
    wanna

--- index.js
+++ index.js
@@ -12,19 +12,40 @@ module.exports = function(file) {
         if (typeof file !== 'string' && typeof file.path !== 'string')
             throw new PluginError('gulp-minify-ejs', 'Missing path in file options for gulp-minify-ejs');
 
-        if (file.isBuffer()) {
+        function trim(str) {
+            if (!str || !str.length)
+                return '';
+            var start, end, i;
+            for (i = str.length - 1; i >= 0; i--) {
+                if (str[i] !== ' ')
+                    break;
+            };
+            //all of chars is empty such as ' '
+            if (i === -1)
+                return '';
+
+            end = i + 1;
+
+            for (i = 0; i < str.length; i++) {
+                if (str[i] !== ' ')
+                    break;
+            };
+            start = i;
+            return str.substring(start, end);
+        }
+
+        function minify(contents) {
             var htmlBuilder = [];
             var inner = false,
-                intag = false,
-                intagin = false,
+                intag = false, //<div> or </div>
+                intagin = false, //<% expr %> ... <% for(var a=1; a "<" 5
                 inscript = false,
-                incss = false; //intagin eg.  <% for(var a=1; a "<" 5
-            var contents = file.contents.toString('utf-8');
+                incss = false;
 
+            var innerTextBuilder = [];
             for (var i = 0; i < contents.length; i++) {
                 var charstr = contents[i];
                 if (charstr === '<') {
-
                     if (contents.substr(i, 7).toLowerCase() === '<script') {
                         inscript = true;
                     }
@@ -33,7 +54,7 @@ module.exports = function(file) {
                         inscript = true;
                     }
 
-                    //maybe <div> or </div> or a < 5
+                    //maybe [<]div> or [<]/div> or a [<] 5
                     if (contents[i + 1] !== '%') {
                         //case a <= 5
                         if (!intagin) {
@@ -41,11 +62,19 @@ module.exports = function(file) {
                         }
                         inner = true;
                     } else {
+                        //> ... [<]% ...
                         if (!inner) {
                             intag = false;
                             inner = true;
                             intagin = true;
                         }
+                    }
+
+                    if (inner && innerTextBuilder.length) {
+                        //debugger;
+                        var innerTextStr = innerTextBuilder.join('');
+                        htmlBuilder.push(trim(innerTextStr))
+                        innerTextBuilder = [];
                     }
                 }
 
@@ -71,16 +100,16 @@ module.exports = function(file) {
                     if (inner) {
                         htmlBuilder.push(charstr);
                     } else {
-                        if (charstr === '\r' || charstr === '\n' || charstr === ' ' || charstr === '\t')
+                        if (charstr === '\r' || charstr === '\n' || charstr === '\t')
                             continue;
-                        htmlBuilder.push(charstr);
+                        innerTextBuilder.push(charstr);
                     }
                 }
-
+                //maybe <...div[>] or </div[>] or <% a [>] 5 %> or <div ...  <% a [>] 5 %>[>]
                 if (charstr === '>') {
                     if (contents[i - 1] !== '%') {
-                        inner = false;
                         intag = false;
+                        inner = false;
                     } else {
                         if (!intag) {
                             inner = false;
@@ -89,7 +118,12 @@ module.exports = function(file) {
                     }
                 }
             }
-            file.contents = new Buffer(htmlBuilder.join(''));
+            return htmlBuilder.join('');
+        }
+
+        if (file.isBuffer()) {
+            var contents = file.contents.toString('utf-8');
+            file.contents = new Buffer(minify(contents));
         }
         this.push(file);
         cb();
